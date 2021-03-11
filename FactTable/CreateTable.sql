82280383137465564bb2062ba93898e12a73d737
yzhao156 <yzhao156@uottawa.ca>
2021-03-10 22:36:47 -0500

    The submitted version

--- FactTable/CreateTable.sql
+++ FactTable/CreateTable.sql
@@ -1,8 +1,8 @@
 
---DROP TABLE Covid-19_Tracking_Fact_Table;
+--DROP TABLE Covid19_Tracking_Fact_Table;
 
 
-create table Covid-19_Tracking_Fact_Table(
+create table Covid19_Tracking_Fact_Table(
 Onset_date_key int,
 Reported_date_key int,
 Test_date_key int,
@@ -14,7 +14,7 @@ Mobility_key int,
 Weather_key int,
 Resolved int,
 Unresolved int,
-Fatal int
+Fatal int,
 foreign key  (Onset_date_key) references Date_dimension(surrogate_key),
 foreign key  (Reported_date_key) references Date_dimension(surrogate_key),
 foreign key  (Test_date_key) references Date_dimension(surrogate_key),
@@ -24,6 +24,6 @@ foreign key  (PHU_location_key) references PHU_Location_dimension(surrogate_key)
 foreign key  (Special_measure_key)  references Special_Measures_dimension(surrogate_key),
 foreign key  (Mobility_key) references Mobility_dimension(surrogate_key),
 foreign key  (Weather_key) references Weather_dimension(surrogate_key)
-)
+);
 
-\COPY Date_dimension FROM '/Users/yi/Desktop/DS_Phase2/FactTable/Covid-19_Tracking_Fact_Table.csv' DELIMITER ',' CSV HEADER;
+\COPY Covid19_Tracking_Fact_Table FROM '/Users/yi/Desktop/DS_Phase2/FactTable/Covid19_Tracking_Fact_Table.csv' DELIMITER ',' CSV HEADER;
--- README.md
+++ README.md
@@ -3,3 +3,6 @@
 ### Probles: <br>
 1. Into sql 
 2. Report & Fill work time table
+
+
+psql -h www.eecs.uottawa.ca -p 15432 -d group_22 -U aaaaaddd  
