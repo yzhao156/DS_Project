ed3a2bde90dc0c30e19ad938d9d72fae78c1faef
yzhao156 <yzhao156@uottawa.ca>
2021-03-20 16:46:45 -0400

    Fact -> 0/1 SQL: partial

---
+++ DS_3_BI/queries.sql
@@ -0,0 +1,48 @@
+select * from covid19_tracking_fact_table
+-- ？？？a.2 1.3 几个query
+
+-- a.1: Each day's case in 2020.11    TODO->count to sum
+SELECT D.day_of_month AS day, COUNT(F.Resolved) AS cases
+FROM covid19_tracking_fact_table F, date_dimension D
+WHERE F.reported_date_key = D.surrogate_key AND
+	D.year = 2020 AND
+	D.Month = 11
+GROUP BY D.day_of_month
+ORDER BY D.day_of_month ASC
+
+
+-- a.2: ？？？
+SELECT D.day_of_month AS day, COUNT(F.Resolved) AS cases
+FROM covid19_tracking_fact_table F, date_dimension D
+WHERE F.reported_date_key = D.surrogate_key AND
+	D.year = 2020 AND
+	D.Month = 11
+GROUP BY D.day_of_month
+ORDER BY D.day_of_month ASC
+
+
+-- b.
+SELECT P.Reporting_PHU, COUNT(Unresolved) AS total_cases 
+FROM covid19_tracking_fact_table F , phu_location_dimension P
+WHERE F.PHU_location_key = P.surrogate_key 
+GROUP BY P.Reporting_PHU
+
+
+
+
+
+
+
+SELECT COUNT(Unresolved) 
+FROM covid19_tracking_fact_table F , phu_location_dimension P
+WHERE F.PHU_location_key = P.surrogate_key AND
+	P.Reporting_PHU_City = 'Toronto'
+GROUP BY Unresolved
+
+
+
+
+
+
+
+
