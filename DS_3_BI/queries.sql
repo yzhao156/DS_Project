3148d99e8a1e007c7df854f8e6bab003f552c9f2
yzhao156 <yzhao156@uottawa.ca>
2021-03-23 15:42:22 -0400

    updated queries

--- DS_3_BI/queries.sql
+++ DS_3_BI/queries.sql
@@ -1,69 +1,50 @@
-select * from covid19_tracking_fact_table
--- ？？？a.2 1.3 几个query
+--1.a.1: Drill down:
+SELECT count(*) as cases, D.year as year, D.quarter as quarter, D.month as month, D.day_of_month as day
+FROM covid19_tracking_fact_table F, date_dimension D, PHU_location_dimension P
+WHERE F.reported_date_key = D.surrogate_key AND 
+P.surrogate_key = F.PHU_location_key AND
+F.unresolved = 1
+GROUP BY (D.year, D.quarter, D.month, D.day_of_month)
+ORDER BY D.year, D.quarter, D.month, day
 
--- a.1: Each day's case in 2020.11    TODO->count to sum
-SELECT D.date AS day, COUNT(*) AS cases
-FROM covid19_tracking_fact_table F, date_dimension D
+
+--1.3.1: Rollup :
+SELECT count(*) as cases, P.reporting_phu_city, D.year as year, D.quarter as quarter, D.month as month
+FROM covid19_tracking_fact_table F, date_dimension D, PHU_location_dimension P
 WHERE F.reported_date_key = D.surrogate_key AND
-	D.year = 2020 AND
-	D.Month = 11
-GROUP BY D.date
-ORDER BY D.date ASC
-
---OR
-
-SELECT D.date AS day, d.month as month, COUNT(*) AS cases
-FROM covid19_tracking_fact_table F, date_dimension D
-WHERE F.reported_date_key = D.surrogate_key AND
-	D.year = 2020 AND
-	D.Month = 11
-GROUP BY GROUPING SETS((D.month),(D.date))
-ORDER BY D.month, D.date 
+P.surrogate_key = F.PHU_location_key AND
+F.unresolved = 1
+GROUP BY P.reporting_phu_city, rollup(D.year, D.quarter, D.month)
+ORDER BY D.year, D.quarter, D.month
 
 
+-- 1.b.1
+SELECT  P.reporting_phu_city, D.date, SUM(F.resolved) as resolved, SUM(F.unresolved) as unresolved, SUM(F.fatal) as fatal
+FROM covid19_tracking_fact_table F, phu_location_dimension P, date_dimension D
+WHERE F.PHU_location_key = P.surrogate_key AND
+	F.reported_date_key = D.surrogate_key AND
+	P.Reporting_PHU = 'Ottawa Public Health'
+GROUP BY (P.reporting_phu_city, D.date)
+ORDER BY D.date
 
--- a.2: ？？？
-SELECT D.date AS day, COUNT(*) AS cases
-FROM covid19_tracking_fact_table F, date_dimension D
-WHERE F.reported_date_key = D.surrogate_key AND
-	D.year = 2020 AND
-	D.Month = 11 AND
-	D.day_of_month <= 7
-GROUP BY D.date
-ORDER BY D.date ASC
-
---OR
-
-SELECT D.date AS day, D.week as week, COUNT(*) AS cases
-FROM covid19_tracking_fact_table F, date_dimension D
-WHERE F.reported_date_key = D.surrogate_key AND
-	D.year = 2020 AND
-	D.month = 11 AND
-	D.week =44
-GROUP BY GROUPING SETS ((D.date),(D.week))
-ORDER BY D.date ASC
-
--- b.1
-SELECT P.Reporting_PHU AS phu, SUM(F.resolved) as resolved, SUM(F.unresolved) as unresolved, SUM(F.fatal) as fatal
-FROM covid19_tracking_fact_table F, phu_location_dimension P
-WHERE F.PHU_location_key = P.surrogate_key 
-GROUP BY (P.Reporting_PHU)
-
-
--- b.2
-SELECT P.Reporting_PHU AS day, SUM(F.resolved) as resolved, SUM(F.unresolved) as unresolved, SUM(F.fatal) as fatal
+-- 1.b.2
+SELECT D.date as date, P.Reporting_PHU AS phu, 
+	SUM(F.resolved) as resolved, 
+	SUM(F.unresolved) as unresolved, 
+	SUM(F.fatal) as fatal
 FROM covid19_tracking_fact_table F,
 	phu_location_dimension P,
+	date_dimension D, 
 	special_measures_dimension S
 WHERE F.PHU_location_key = P.surrogate_key AND
 	F.special_measure_key = S.surrogate_key AND
+	F.reported_date_key = D.surrogate_key AND
 	S.title = 'Lockdown'
-GROUP BY (P.Reporting_PHU)
+GROUP BY (phu,date)
+ORDER BY date
 
 
-
-
--- c.1
+-- 1.c.1
 SELECT D.month as month, L.reporting_phu_city as city, SUM(F.fatal) AS fatal_cases
 FROM covid19_tracking_fact_table F, 
 	date_dimension D, 
@@ -77,13 +58,12 @@ WHERE F.reported_date_key = D.surrogate_key AND
 GROUP BY (D.month, L.reporting_phu_city)
 ORDER BY D.month
 
--- c.2
 
-
+-- 1.c.2
 SELECT L.reporting_phu_city,
 	(case when M.parks_percent_change_from_baseline>41 then true else false end) as park,
 	(case when M.transit_stations_percent_change_from_baseline>-54 then true else false end) as transit,
-	SUM(F.unresolved) AS unsolved_cases
+	SUM(F.unresolved) AS unresolved_cases
 FROM covid19_tracking_fact_table F, 
 	mobility_dimension M, 
 	phu_location_dimension L
@@ -129,26 +109,14 @@ WHERE F.reported_date_key = D.surrogate_key
 GROUP BY D.month
 ORDER BY year, D.month ASC
 
--- d.3 
-
-SELECT S.title AS title, CASE WHEN P.reporting_phu_city='Ottawa' THEN 'Ottawa' else 'Toronto' END AS city, SUM(F.resolved) as resolved, SUM(F.unresolved) as unresolved, SUM(F.fatal) as fatal
+-- 1.d.3 Measure
+SELECT S.title AS title, SUM(F.resolved) as resolved, SUM(F.unresolved) as unresolved, SUM(F.fatal) as fatal
 FROM covid19_tracking_fact_table F,
 	phu_location_dimension P,
 	special_measures_dimension S
 WHERE F.PHU_location_key = P.surrogate_key AND
 	F.special_measure_key = S.surrogate_key 
-GROUP BY (S.title, city)
-ORDER BY city, title
-
--- or
-
-SELECT S.title AS title, P.reporting_phu_city AS city, SUM(F.resolved) as resolved, SUM(F.unresolved) as unresolved, SUM(F.fatal) as fatal
-FROM covid19_tracking_fact_table F,
-	phu_location_dimension P,
-	special_measures_dimension S
-WHERE F.PHU_location_key = P.surrogate_key AND
-	F.special_measure_key = S.surrogate_key 
-GROUP BY (S.title,P.reporting_phu_city)
+GROUP BY (S.title)
 ORDER BY title
 
 
@@ -221,5 +189,17 @@ GROUP BY (D.day_of_month, S.title, D.date)
 ORDER BY D.day_of_month ASC
 
 
+-- PART3
+
+
+SELECT D.date as date, L.reporting_phu_city as city, SUM(F.resolved) as resolved, SUM(F.unresolved) as unresolved, SUM(F.fatal) as fatal
+FROM covid19_tracking_fact_table F, 
+	date_dimension D, 
+	phu_location_dimension L
+WHERE F.reported_date_key = D.surrogate_key AND
+	F.PHU_location_key = L.surrogate_key AND
+	L.reporting_phu_city = 'Ottawa' 
+GROUP BY D.date,city
+ORDER BY D.date
 
 
